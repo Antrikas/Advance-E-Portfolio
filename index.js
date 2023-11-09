efed6ffd347a965e7399a750a789f05dd9a20551
Antrikas <andreas.antoniades17@gmail.com>
2023-11-09 16:35:08 +0200

    aep

---
+++ index.js
@@ -0,0 +1,23 @@
+function contact(event) {
+    event.preventDefault ();
+    const loading = document.querySelector ('.modal__overlay--loading');
+    const success = document.querySelector ('.modal__overlay--success');
+    loading.classList =+ " modal__overlay--visible"
+    emailjs
+    .sendForm(
+        'service_27ap62g',
+        'template_2vn9wxn',
+        event.target, 
+        '2AkFvnWfcb3Bsoykk'
+    ).then(() => {
+    
+        loading.classList.remove ("modal__overlay--visible");
+        success.classList += " modal__overlay--visible";
+    }).catch (() => {
+        loading.classList.remove ("modal__overlay--visible");
+        alert(
+            "The email service is temporary unavailable. Please contact me directly at andreas.antoniades17@gmail.com "
+        );
+    })
+
+}
